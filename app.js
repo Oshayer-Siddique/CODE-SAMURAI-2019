c22bdb1faf8834d2f98f1b9cec8182d594590e29
Oshayer Siddique <oshayer.siddique2001@gmail.com>
2024-02-09 17:26:55 +0600

    seven

--- app.js
+++ app.js
@@ -39,12 +39,6 @@ mongoose
   });
 
 
-  // mongoose.connect('mongodb://localhost:27017/mydatabase', {
-  //   //useNewUrlParser: true,
-  //   //useUnifiedTopology: true
-  // })
-  // .then(() => console.log('MongoDB connected'))
-  // .catch(err => console.error('MongoDB connection error:', err));
 
 
   app.use(cookieParser());
